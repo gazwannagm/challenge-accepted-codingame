226a02f23be525456420d07d0ccdb3064a64f1c1
Gazwan Nagm <33843358+gazwannagm@users.noreply.github.com>
2022-07-10 20:30:00 +0300

    Update and rename Horse-racing Duals.py to Horse-racing Duals.ts

---
+++ Week02/Horse-racing Duals.ts
@@ -0,0 +1,24 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+let N: number = parseInt(readline());
+const values: number[] = [];
+values.length = N;
+
+for (let i = 0; i < N; i++) {
+  const pi = parseInt(readline());
+  values[i] = pi
+}
+values.sort((a, b) => a - b); // sort
+let diff = 10000000 
+for (let j = 1; j < N; j++) {
+  if ((values[j] - values[j - 1]) < diff) {
+    diff = values[j] - values[j - 1]
+  }
+
+}
+console.log(diff);
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
