7b3490a7b645edcfd1a551044aab933f42fffbfd
Gazwan Nagm <33843358+gazwannagm@users.noreply.github.com>
2022-07-10 20:30:17 +0300

    Update Horse-racing Duals.ts

--- Week02/Horse-racing Duals.ts
+++ Week02/Horse-racing Duals.ts
@@ -11,7 +11,7 @@ for (let i = 0; i < N; i++) {
   values[i] = pi
 }
 values.sort((a, b) => a - b); // sort
-let diff = 10000000 
+let diff = 10000000;
 for (let j = 1; j < N; j++) {
   if ((values[j] - values[j - 1]) < diff) {
     diff = values[j] - values[j - 1]
